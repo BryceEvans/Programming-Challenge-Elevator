e9b1441780e9ad01baa9dd4587d190800288bf65
BryceEvans <evans.bryce@gmail.com>
2019-07-11 08:51:07 -0600

    classes built out.

--- elevator.py
+++ elevator.py
@@ -12,13 +12,15 @@ class Controller:
 
 class Elevator(Controller):
 
-    def __init__(self, area, elevators, floors, floorCall, reportFloor, openDoor, floorRequest, tripAmount, maintenanceNeeded):
+    def __init__(self, area, elevators, floors, floorCall, elevatorNumber, reportFloor, openDoor, floorRequest, tripAmount, maintenanceNeeded):
         super().__init__(area, elevators, floors, floorCall)
+        # this refers to which elevator is it (int)
+        self.elevatorNumber = elevatorNumber
         # this refers to the floor the elevator is on (int)
         self.reportFloor = reportFloor
         # this reports door open or closed (boolean)
         self.openDoor = openDoor
-        #
+        # this will take a floor request from the controller
         self.floorRequest = floorRequest
         # this will be a counter to track the number of trips (int, counter)
         self.tripAmount = tripAmount
@@ -26,3 +28,19 @@ class Elevator(Controller):
         self.maintenanceNeeded = maintenanceNeeded
 
     
+class OutsidePanel(Controller):
+    
+    def __init__(self, area, elevators, floors, floorCall, floorNumber, upCall, downCall):
+        super().__init__(area, elevators, floors, floorCall)
+        # this is the floor the outside panel is on (int)
+        self.floorNumber = floorNumber
+        # this is if the panel calls for an elevator to go up (boolean)
+        self.upCall = upCall
+        # this is if the panel calls for an elevator to go down (boolean)
+        self.downCall = downCall
+
+class InsidePanel(Elevator):
+    def __init__(self, area, elevators, floors, floorCall, elevatorNumber, reportFloor, openDoor, floorRequest, tripAmount, maintenanceNeeded, floorCalled):
+        super().__init__(area, elevators, floors, floorCall, elevatorNumber, reportFloor, openDoor, floorRequest, tripAmount, maintenanceNeeded)
+        # this will make a request to which floor the person inside wants to go to. It will send it to the controller
+        self.floorCalled = floorCalled
